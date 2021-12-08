794f68ecc3b8c6c068e4815176158fa6e2d2201b
TanmanG <tgood2001@hotmail.com>
2021-12-08 15:28:06 -0800

    Updated comment to explain source of imprecision.

--- Week 1/Day 2/Week1Day2_Float.cpp
+++ Week 1/Day 2/Week1Day2_Float.cpp
@@ -3,9 +3,9 @@
 #include <math.h>
 
 int main() {
-	// Precision is lost with floats as the size grows.
+	// Precision is lost with floats as the size of the number grows, as the bits for exponent shrink.
 	float x1 = 2.0f / 3.0f;
 	std::cout << std::setprecision(32) << std::fixed << x1 << '\n';
-	float x2 = 1000000.0f + 2.0f / 3.0f;
+	float x2 = 1000000.0f + x1;
 	std::cout << std::setprecision(32) << std::fixed << x2 << '\n';
 }
