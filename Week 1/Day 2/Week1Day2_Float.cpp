79c3ec806dd169d60da5ce33c0ccc7b978d7e44e
TanmanG <tgood2001@hotmail.com>
2021-12-08 15:22:42 -0800

    Added float precision script.

--- Week 1/Day 2/Week1Day2_Float.cpp
+++ Week 1/Day 2/Week1Day2_Float.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <iomanip>
+#include <math.h>
 
 int main() {
-	// Trying to figure out how to force a floating point precision error to show...
+	// Precision is lost with floats as the size grows.
+	float x1 = 2.0f / 3.0f;
+	std::cout << std::setprecision(32) << std::fixed << x1 << '\n';
+	float x2 = 1000000.0f + 2.0f / 3.0f;
+	std::cout << std::setprecision(32) << std::fixed << x2 << '\n';
 }
