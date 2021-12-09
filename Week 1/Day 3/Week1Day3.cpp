7504b131793278100b0fd3aff7931def9bf5f231
TanmanG <tgood2001@hotmail.com>
2021-12-09 14:17:42 -0800

    Added comments to prompt 1 code and added prompt 3 & 4 answers.

--- Week 1/Day 3/Week1Day3.cpp
+++ Week 1/Day 3/Week1Day3.cpp
@@ -3,10 +3,14 @@
 #include <string>
 
 int main() {
+	// Declare an array and vector, initializing the array.
 	float myArr [3] = { 1.0f, 2.0f, 3.0f};
-	size_t size = 0;
 	std::vector<float> myVec;
 
+	size_t size = 0;
+
+	// Iterate through the array and push each element into the vector, incrementing the tracked size of the array
+	// for later.
 	for (float val : myArr) {
 		std::cout << val << ' ';
 		myVec.push_back(val);
@@ -14,6 +18,9 @@ int main() {
 	}
 	std::cout << '\n';
 
+	// Using the tracked array size from earlier, iterate through each element of the vector and print each one.
+	// Alternate solution would be to just use another iterator for-loop but that'd take some creative expression
+	// away.
 	for (size_t i = 0; i < size; i++) {
 		std::cout << myVec.at(i) << ' ';
 	}
---
+++ Week 1/Day 3/Week1Day3_MixedArr.cpp
@@ -0,0 +1,13 @@
+
+int main() {
+	
+}
+
+// Prompt 3A. Arrays are great choices for working with fixed data sizes, while vectors are a better choice for more
+// dynamic sets of information (as they can grow/shrink). Due to the limited and structured nature of Arrays, they
+// are easy to sort and always have perfect random access efficiency. Their downside is that they are immutable in
+// size, can only store one type, and always uses a fixed number of elements (i.e. the declared size of the array).
+
+// Prompt 4. push_back() is extremely efficient as it makes use of vector::back, which is a pointer to the back-most
+// element. Furthermore, there is no re-shuffling of other elements required freeing the efficiency to O(1).
+
