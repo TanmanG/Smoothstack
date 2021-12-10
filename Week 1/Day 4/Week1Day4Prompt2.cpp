b8761727b5e477508186d683c37c2a9c9bc2bba9
TanmanG <tgood2001@hotmail.com>
2021-12-10 15:28:58 -0800

    Added Prompt 2 and Prompt 5.

--- Week 1/Day 4/Week1Day4Prompt2.cpp
+++ Week 1/Day 4/Week1Day4Prompt2.cpp
@@ -1,20 +1,41 @@
-// Prompt 2.
 #include <iostream>
 #include <typeinfo>
+#include <string>
 
 int main() {
+	// Prompt 2
 	float xFloat = 1.0;
 	int xInt = 1;
 	char xChar = 'a'; // Value is 97
 	bool xBool = true;
 
 	// Any of the operations involving float are casted into floats with the rest casting into integers.
-	std::cout << "Type of Float + Int: "  << typeid(xFloat + xInt).name() << '\n';
-	std::cout << "Type of Float + Char: " << typeid(xFloat + xChar).name() << '\n';
-	std::cout << "Type of Float + Bool: " << typeid(xFloat + xBool).name() << '\n';
-	std::cout << "Type of Int + Char: " << typeid(xInt + xChar).name() << '\n';
-	std::cout << "Type of Int + Bool: " << typeid(xInt + xBool).name() << '\n';
-	std::cout << "Type of Char + Bool: " << typeid(xChar + xBool).name() << '\n';
+	std::cout << "----- " << "F " << "I " <<  "C " << "B" << '\n';
+	std::cout << "Float "  << typeid(xFloat + xFloat).name() << " " << typeid(xFloat + xInt).name() << " "
+				<< typeid(xFloat + xChar).name() << " " << typeid(xFloat + xBool).name() << '\n';
+	std::cout << "Int   " << typeid(xInt + xFloat).name() << " " << typeid(xInt + xInt).name() << " "
+				<< typeid(xInt + xChar).name() << " " << typeid(xInt +xBool).name() << '\n';
+	std::cout << "Char  " << typeid(xChar + xFloat).name() << " " << typeid(xChar + xInt).name() << " "
+				<< typeid(xChar + xChar).name() << " " << typeid(xChar + xBool).name() << '\n';
+	std::cout << "Bool  " << typeid(xBool + xFloat).name() << " " << typeid(xBool + xInt).name() << " "
+				<< typeid(xBool + xChar).name() << " " << typeid(xBool + xBool).name() << '\n';
+
+	// Prompt 5
+	bool tfAND = (true && false);
+	bool tfOR = (true || false);
+	bool ttAND = (true && true);
+	bool ttOR = (true && true);
+	bool ffAND = (false && false);
+	bool ffOR = (false || false);
+	bool tNOT = (!true);
+	bool fNOT = (!false);
+
+	std::cout << "----- " << "T " << "F" << '\n';
+	std::cout << "T-And " << ttAND << " " << tfAND << '\n';
+	std::cout << "F-And " << tfAND << " " << ffAND << '\n';
+	std::cout << "T-Or  " << ttOR << " " << tfOR << '\n';
+	std::cout << "F-Or  " << tfOR << " " << ffOR << '\n';
+	std::cout << "Not   " << tNOT << " " << fNOT << '\n';
 }
 
 /*
@@ -22,4 +43,6 @@ Prompt 3.
 Unary (One operand): ++, --, - (negative), sizeof(), ! (not), Address (&)
 Binary (Two operands): +, -, *, /, % (mod.)
 Ternary (Three operands): ? and : (conditional operator)
+
+Prompt 4. See Prompt 4 file.
 */
