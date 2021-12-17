d534031d29ae2ecd211805c4c4bcaf5d694c3398
TanmanG <tgood2001@hotmail.com>
2021-12-17 14:06:47 -0800

    Implemented solution for matrix multiplication.

--- Week 1/Day 5/matrixmulti.cpp
+++ Week 1/Day 5/matrixmulti.cpp
@@ -4,33 +4,83 @@
 
 int main() {
 	// Generate two matrixes that contain random numbers.
-	int matrixA[3][6];
-	int matrixB[6][3];
+	int matrixA[6][3];
+	int matrixB[3][6];
 
-	for (int i = 0; i < sizeof(matrixA) / sizeof(matrixA[0]); i++) {
-		for (int h = 0; h < sizeof(matrixA) / sizeof(int); h++) {
-			matrixA[i][h] = rand() % 10;
+	// Create variables os matrix sizes to clean up code performance and readability.
+	int matrixAX = sizeof(matrixA) / sizeof(matrixA[0]);
+	int matrixAY = sizeof(matrixA[0]) / sizeof(int);
+	int matrixBX = sizeof(matrixB) / sizeof(matrixB[0]);
+	int matrixBY = sizeof(matrixB[0]) / sizeof(int);
+
+	if (matrixAX != matrixBY || matrixAY != matrixBX) {
+		std::cout << "Matrix dimensions are invalid (Matrix A's height and width must match Matrix B's width and height respectively)!" << '\n';
+		return 0;
+	}
+
+	// Populate Matrix A and B with random integers.
+	// Iterate through each row (Matrix A) and column (Matrix B).
+	for (int aRow = 0; aRow < matrixAX; aRow++) {
+		// Iterate through each column (Matrix A) and row (Matrix B)
+		for (int aCol = 0; aCol < matrixAY; aCol++) {
+			matrixA[aCol][aRow] = rand() % 10;
+			matrixB[aRow][aCol] = rand() % 10;
 		}
 	}
 
-	// Create a third matrix of the final size.
-	int matrixC[sizeof(matrixA) / sizeof(matrixA[0])][sizeof(matrixB[0]) / sizeof(int)];
+	int matrixC[matrixAY][matrixBX];
 
-	// calculate dot product.
+	// Holder for the current dot product.
 	int currTotal = 0;
-	// Iterate through each row in matrix A.
-	for (size_t matrixARow = 0; matrixARow < sizeof(matrixA) / sizeof(matrixA[0]); matrixARow++) {
-		// Iterate through each column in matrix B.
-		for (size_t matrixBColumn = 0; matrixBColumn < sizeof(matrixB[0]) / sizeof(int); matrixBColumn++) {
-			// Iterate through each element within matrix A and B.
-			for (size_t element = 0; element < sizeof(matrixA) / sizeof(matrixA[0]); element++) {
-				currTotal += matrixA[element][matrixARow] * matrixB[matrixBColumn][element];
+
+	// Iterate through each row of Matrix A.
+	for (int n = 0; n < matrixAY; n++) {
+		// Iterate through each column of Matrix B.
+		for (int m = 0; m < matrixBX; m++) {
+			// Iterate through each element.
+			for (int i = 0; i < matrixAX; i++) {
+				currTotal += (matrixA[n][i] * matrixB[i][m]);
 			}
-			matrixC[matrixARow][matrixBColumn] = currTotal;
-			currTotal = 0;
+		matrixC[n][m] = currTotal;
+		currTotal = 0;
 		}
 	}
-	std::cout << matrixA[0][0] << '\n';
+
+	std::cout << "Matrix A" << '\n';
+	// Print Matrix A.
+	// Iterate through each row.
+	for (int aRow = 0; aRow < matrixAY; aRow++) {
+		// Iterate through each column.
+		for (int aCol = 0; aCol < matrixAX; aCol++) {
+			std::cout << matrixA[aRow][aCol] << " ";
+		}
+		std:: cout << '\n';
+	}
+	std::cout << '\n';
+
+	std::cout << "Matrix B" << '\n';
+	// Print Matrix B.
+	// Iterate through each row.
+	for (int bRow = 0; bRow < matrixBY; bRow++) {
+		// Iterate through each column.
+		for (int bCol = 0; bCol < matrixBX; bCol++) {
+			std::cout << matrixB[bRow][bCol] << " ";
+		}
+		std::cout << '\n';
+	}
+	std::cout << '\n';
+
+	std::cout << "Matrix C" << '\n';
+	// Print Matrix C.
+	// Iterate through each row.
+	for (int cRow = 0; cRow < matrixAY; cRow++) {
+		// Iterate through each column.
+		for (int cCol = 0; cCol < matrixBX; cCol++) {
+			std::cout << matrixC[cRow][cCol] << " ";
+		}
+		std::cout <<'\n';
+	}
+	std::cout << '\n';
 	return 0;
 }
 
